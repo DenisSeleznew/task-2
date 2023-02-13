b8c63f2a4b56dd7d576a0dbe5ffdd8d1de38b366
DenisSeleznew <84568286+DenisSeleznew@users.noreply.github.com>
2023-02-13 20:50:06 +0300

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import styles from 'rollup-plugin-styles';
+import image from 'rollup-plugin-img';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+export default {
+	input: './src/index.js',
+	output: {
+		file: './build/bundle.js',
+		format: 'iife',
+		name: 'build',
+	},
+	watch: {
+		exclude: ['node_modules/**'],
+	},
+	plugins: [
+		styles(),
+		image(),
+		babel({ babelHelpers: 'bundled' }),
+		serve({
+			contentBase: './',
+			open: true,
+			port: 8000,
+		}),
+		livereload(),
+	],
+};
